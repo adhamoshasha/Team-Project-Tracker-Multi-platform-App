da0b8533fd8c62e881c2e194dba0c43bbe126a35
Talal Mahdy <talal.mahdy96@gmail.com>
2018-02-02 23:36:38 +0300

    Add files via upload

---
+++ Application Files/Calendar.UWP/Effects/FocusEffectWin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using EffectsDemo.Droid;
+using Windows.UI.Xaml.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ResolutionGroupName("MyCompany")]
+[assembly: ExportEffect(typeof(FocusEffectWin), "FocusEffectWin")]
+namespace EffectsDemo.Droid
+{
+    public class FocusEffectWin : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                var UIelement = ((TextBox)Control);
+                var thickness = UIelement.Padding;
+                thickness.Left = 0;
+                thickness.Right = 0;
+                UIelement.Padding = thickness;
+                UIelement.MinWidth = 0;
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+
+
+    }
+}
